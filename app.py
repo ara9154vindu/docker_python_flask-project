56add37f2e66731aad805726569c3e2412d7ae78
Aravind Cheggoju <162151965+ara9154vindu@users.noreply.github.com>
2024-12-06 13:48:49 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-import os
 
+# Create an instance of the Flask class
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Flask inside Docker!!"
+# Define the route for the home page
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
